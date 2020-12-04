5daa87071a865bc8cfaaab8d32fba647927543b1
Luis Rondow <luiseduardorondow@gmail.com>
2020-12-04 14:19:57 -0300

    Add Deployment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,24 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
+import https from "https";
 
-console.log(process.env)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.defaults.timeout = 10000000;
 
 api.interceptors.request.use(
   async (config) => {
-    const token = sessionStorage.getItem('@token');
+    const token = sessionStorage.getItem("@token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => Promise.reject(error),
+  (error) => Promise.reject(error)
 );
 
-api.defaults.headers.post['Content-Type'] = 'application/json';
+api.defaults.headers.post["Content-Type"] = "application/json";
+
+axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
 export default api;
